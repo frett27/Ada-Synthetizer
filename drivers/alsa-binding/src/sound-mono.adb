1e25755cd9ff87102c03c60fce5ecd5c86fbdc45
Patrice Freydiere <frett27@gmail.com>
2018-09-01 20:14:54 +0000

    adjust linux

--- drivers/alsa-binding/src/sound-mono.adb
+++ drivers/alsa-binding/src/sound-mono.adb
@@ -228,20 +228,25 @@ package body Sound.Mono is
         return ALSA.snd_pcm_sframes_t;
       pragma Import (C, snd_pcm_writei);
 
+      --  function snd_pcm_drain (pcm   : in    Line_Type)
+      --    return Interfaces.C.int;
+      --  pragma Import (C, snd_pcm_drain);
+
       use type Sound.ALSA.snd_pcm_sframes_t;
+      --  use type Interfaces.C.int;
       Written_Frame_Count : Sound.ALSA.snd_pcm_sframes_t;
-      RetCode : Interfaces.C.int;
+      --  RetCode : Interfaces.C.int;
    begin
-      RetCode := snd_pcm_drain();
-      if RetCode < 0 then
-          raise Program_Error with 
-           "snd_pcm_drain failed: " & RetCode'Img;
-      end if;
+      --  RetCode := snd_pcm_drain (Line);
+      --  if RetCode < 0 then
+      --     raise Program_Error with
+      --       "snd_pcm_drain failed: " & Integer'Image (Integer (RetCode));
+      --  end if;
 
       Written_Frame_Count := snd_pcm_writei (pcm    => Line,
                                              buffer => Item,
                                              size   => Item'Length);
-    
+
       if Written_Frame_Count < 0 then
          raise Program_Error with
            "snd_pcm_writei failed: " & Written_Frame_Count'Img;
