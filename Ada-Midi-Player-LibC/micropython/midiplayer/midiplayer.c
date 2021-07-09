aa0fa3e3ff352d5d090b06d0cdd482368b31f2e0
Patrice Freydiere <frett27@gmail.com>
2021-07-09 07:55:02 +0200

    improve mp binding

--- Ada-Midi-Player-LibC/micropython/midiplayer/midiplayer.c
+++ Ada-Midi-Player-LibC/micropython/midiplayer/midiplayer.c
@@ -6,18 +6,39 @@ extern void midiplayerlibcinit();
 extern int midiplayerlib_init(void);
 extern void *midiplayerlib_loadsoundbank(const char* filename);
 extern int midiplayerlib_play(const char* filename);
+extern int midiplayerlib_stop(void);
 extern int midiplayerlib_definesoundbank(const void* soundbank);
 extern int midiplayerlib_activatebank(const char* bank);
 extern int midiplayerlib_deactivatebank(const char* bank);
 extern int midiplayerlib_changetempofactor(const double f);
 
+extern float midiplayerlib_play_currentstreamposition();
+extern float midiplayerlib_play_currentstreamlength();
+extern int midiplayerlib_isplaying(); // 1 true , 0 false
 
+extern int midiplayerlib_activatefeature(const char* feature, int activated);
+
+/**
+ * Init the synthetizer
+ */ 
 STATIC mp_obj_t midiplayer_init() {
     midiplayerlibcinit();
     int r = midiplayerlib_init();
     if (r != 0) {
         // raise py exception
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_Init_Failed));
+    }
+	return mp_const_none;
+}
+
+
+STATIC mp_obj_t midiplayer_activatefeature(mp_obj_t feature, mp_obj_t activated) {
+	const char * sfeature = mp_obj_str_get_str(feature); 
+    const int act = mp_obj_int_get_checked(activated);
+
+	int resultCode = midiplayerlib_activatefeature(sfeature, act);
+    if (resultCode != 0) {
+        mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_Fail_to_change_feature));
     }
 	return mp_const_none;
 }
@@ -38,8 +59,8 @@ STATIC mp_obj_t midiplayer_definesoundbank() {
          mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_No_loaded_bank));
          return mp_const_none;
     }
-	int r = midiplayerlib_definesoundbank(currentsb);
-    if (r != 0) {
+	int resultCode = midiplayerlib_definesoundbank(currentsb);
+    if (resultCode != 0) {
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_Error_define_sound_bank));
     }
 	return mp_const_none;
@@ -47,8 +68,8 @@ STATIC mp_obj_t midiplayer_definesoundbank() {
 
 STATIC mp_obj_t midiplayer_activatebank(mp_obj_t bank) {
     const char * sbank = mp_obj_str_get_str(bank); 
-	int r = midiplayerlib_activatebank(sbank);
-    if (r != 0) {
+	int resultCode = midiplayerlib_activatebank(sbank);
+    if (resultCode != 0) {
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_api_failed));
     }
 	return mp_const_none;
@@ -56,18 +77,27 @@ STATIC mp_obj_t midiplayer_activatebank(mp_obj_t bank) {
 
 STATIC mp_obj_t midiplayer_deactivatebank(mp_obj_t bank) {
     const char * sbank = mp_obj_str_get_str(bank); 
-	int r = midiplayerlib_deactivatebank(sbank);
-    if (r != 0) {
+	int resultCode = midiplayerlib_deactivatebank(sbank);
+    if (resultCode != 0) {
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_api_failed));
     }
 	return mp_const_none;
 }
 
 
+STATIC mp_obj_t midiplayer_stop() {
+    int resultCode = midiplayerlib_stop();
+    if (resultCode != 0) {
+        mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_api_failed));
+    }
+    return mp_const_none;
+}
+
+
 STATIC mp_obj_t midiplayer_play(mp_obj_t filename) {
 	const char * sFileName = mp_obj_str_get_str(filename); 
-    int r = midiplayerlib_play(sFileName);
-    if (r != 0) {
+    int resultCode = midiplayerlib_play(sFileName);
+    if (resultCode != 0) {
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_Play_Failed));
     }
     return mp_const_none;
@@ -75,12 +105,28 @@ STATIC mp_obj_t midiplayer_play(mp_obj_t filename) {
 
 STATIC mp_obj_t midiplayer_changetempofactor(mp_obj_t tempo) {
 	double tempofactor = mp_obj_float_get(tempo); 
-    int r = midiplayerlib_changetempofactor(tempofactor);
-    if (r != 0) {
+    int resultCode = midiplayerlib_changetempofactor(tempofactor);
+    if (resultCode != 0) {
         mp_raise_ValueError(MP_ROM_QSTR(MP_QSTR_api_failed));
     }
     return mp_const_none;
 }
+
+STATIC mp_obj_t midiplayer_currentstreamposition() {
+    float r = midiplayerlib_play_currentstreamposition();
+    return mp_obj_new_float(r);
+}
+
+STATIC mp_obj_t midiplayer_currentstreamlength() {
+    float r = midiplayerlib_play_currentstreamlength();
+    return mp_obj_new_float(r);
+}
+
+STATIC mp_obj_t midiplayer_isplaying() {
+    int r = midiplayerlib_isplaying();
+    return mp_obj_new_int(r);
+}
+
 
 /*
  -- Init
@@ -122,10 +168,15 @@ STATIC mp_obj_t midiplayer_changetempofactor(mp_obj_t tempo) {
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_init_obj, midiplayer_init);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(midiplayer_loadsoundbank_obj, midiplayer_loadsoundbank);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(midiplayer_play_obj, midiplayer_play);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_stop_obj, midiplayer_stop);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_definesoundbank_obj, midiplayer_definesoundbank);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(midiplayer_activatebank_obj, midiplayer_activatebank);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(midiplayer_deactivatebank_obj, midiplayer_deactivatebank);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(midiplayer_changetempofactor_obj, midiplayer_changetempofactor);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_currentstreamlength_obj, midiplayer_currentstreamlength);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_currentstreampos_obj, midiplayer_currentstreamposition);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(midiplayer_activatefeature_obj, midiplayer_activatefeature);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(midiplayer_isplaying_obj, midiplayer_isplaying);
 
 
 
@@ -139,19 +190,27 @@ STATIC const mp_rom_map_elem_t midiplayer_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&midiplayer_init_obj) },
     { MP_ROM_QSTR(MP_QSTR_loadsoundbank), MP_ROM_PTR(&midiplayer_loadsoundbank_obj) },
     { MP_ROM_QSTR(MP_QSTR_play), MP_ROM_PTR(&midiplayer_play_obj) },
+    { MP_ROM_QSTR(MP_QSTR_stop), MP_ROM_PTR(&midiplayer_stop_obj) },
     { MP_ROM_QSTR(MP_QSTR_definesoundbank), MP_ROM_PTR(&midiplayer_definesoundbank_obj) },
     { MP_ROM_QSTR(MP_QSTR_activatebank), MP_ROM_PTR(&midiplayer_activatebank_obj) },
     { MP_ROM_QSTR(MP_QSTR_deactivatebank), MP_ROM_PTR(&midiplayer_deactivatebank_obj) },
     { MP_ROM_QSTR(MP_QSTR_changetempofactor), MP_ROM_PTR(&midiplayer_changetempofactor_obj) },
+    { MP_ROM_QSTR(MP_QSTR_playstreamlength), MP_ROM_PTR(&midiplayer_currentstreamlength_obj) },
+    { MP_ROM_QSTR(MP_QSTR_playstreamposition), MP_ROM_PTR(&midiplayer_currentstreampos_obj) },
+    { MP_ROM_QSTR(MP_QSTR_feature_set), MP_ROM_PTR(&midiplayer_activatefeature_obj) },
+    { MP_ROM_QSTR(MP_QSTR_isplaying), MP_ROM_PTR(&midiplayer_isplaying_obj) },
+    
 };
 STATIC MP_DEFINE_CONST_DICT(midiplayer_module_globals, midiplayer_module_globals_table);
 
 // Define module object.
-const mp_obj_module_t midiplayer_user_cmodule = {
+const mp_obj_module_t midiplayer_cmodule = {
     .base = { &mp_type_module },
     .globals = (mp_obj_dict_t *)&midiplayer_module_globals,
 };
 
 // Register the module to make it available in Python.
-MP_REGISTER_MODULE(MP_QSTR_midiplayer, midiplayer_user_cmodule, MODULE_MIDIPLAYER_ENABLED);
+MP_REGISTER_MODULE(MP_QSTR_midiplayer, midiplayer_cmodule, MODULE_MIDIPLAYER_ENABLED);
+
+
 
